2a7b423aab7ca4b46c2697bd0b76dec8cedfcd26
Zachary Perlmutter <zrp200@gmail.com>
2014-12-07 14:27:59 -0800

    Update and rename player.rb to lib/player.rb
    * Moved to library
    * Added documentation

--- lib/player.rb
+++ lib/player.rb
@@ -2,14 +2,12 @@ class Player
 
   attr_reader :name
 
-  def initialize(name)
-    @name = name
-  end
+  def initialize(name), @name = name, end
 
   def get_move(game, mark)
     puts "#{@name}, Enter a move: "
     pos = gets.chomp.split(',').map(&:to_i)
-    until valid_move?(pos)
+    until valid_move? pos
       puts "Invalid move, enter 2 numbers between 0 and 2, separated by a comma. e.g: 0,1"
       pos = gets.chomp.split(',').map(&:to_i)
     end
@@ -19,8 +17,7 @@ class Player
 
   private
 
-  def valid_move?(pos)
-    #check if move is on the board
+  def valid_move?(pos) # Checks if move is on the board
     pos.all? { |coord| (0..2).include?(coord) } && pos.length == 2
   end
 
