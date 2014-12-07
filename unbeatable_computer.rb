229f99e7d9216f7e737e310c6e9bda937c4b920d
Zachary Perlmutter <zrp200@gmail.com>
2014-12-07 14:33:12 -0800

    Update unbeatable_computer.rb
    * moved to library
    * added documentation
    * removed excess ()s
    * simplified code

--- unbeatable_computer.rb
+++ unbeatable_computer.rb
@@ -1,20 +1,13 @@
-require_relative 'computer_player'
-require_relative 'game_node'
+require_relative 'computer_player.rb'
+require_relative 'game_node.rb'
 
-class UnbeatableComputer < ComputerPlayer
+class UnbeatableComputer < ComputerPlayer # The unbeatable CPU
 
-  def initialize(name)
-    super(name)
-  end
+  def initialize(name); super name; end
 
-  def get_move(game, mark)
-    node = TTTNode.new(game.board, mark)
-
-    pos_moves = node.children
-    #any possible winning moves?
-    node = pos_moves.find{ |child| child.winning_node?(mark) }
+  def get_move(game, mark)  # Checks to see if there are any possible winning moves, and if there aren't any, takes a move that is not a losing move.
+    node, pos_moves, node = TTTNode.new(game.board, mark), node.children, pos_moves.find{ |child| child.winning_node?(mark) }
     return node.prev_move if node
-    #take a move that isn't a losing move
     node = pos_moves.find{ |child| !child.losing_node?(mark) }
     return node.prev_move if node
   end
